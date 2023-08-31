f45773a8dc7a82dc8558a1e10155cc0a0ded8add
tonnsama123 <tonton3tonsama0@gmail.com>
2023-08-31 19:00:34 +0900

    first commit

---
+++ src/main/java/jp/co/internous/easylogin/model/form/MemberForm.java
@@ -0,0 +1,43 @@
+package jp.co.internous.easylogin.model.form;
+
+import java.io.Serializable;
+
+public class MemberForm implements Serializable{
+
+	private int id;
+	private  String userName;
+	private  String password;
+	private  String fullName;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id=id;
+	}
+	
+	public  void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	public  String getUserName() {
+		return userName;
+	}
+	
+	public  void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+
+	public void setFullName(String fullName) {
+		this.fullName=fullName;
+	}
+	
+	public   String getFullName() {
+		return fullName;
+	}
+}
